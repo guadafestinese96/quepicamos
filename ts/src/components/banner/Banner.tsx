2e23d80442d15d50f53df15c562950424b7ed468
guadafestinese <guadalupefestinese@gmail.com>
2025-03-21 18:29:18 -0300

    .

--- ts/src/components/banner/Banner.tsx
+++ ts/src/components/banner/Banner.tsx
@@ -4,14 +4,14 @@ export const Banner = () => {
   return (
     <div className="bannerContainer">
       <img
-        src="../../public/deliveryApp.jpg"
+        src="images/deliveryApp.jpg"
         alt="deliveryApp"
         className="bannerImg"
       />
       <div className="pContainer">
         <p className="p1">Tus comidas preferidas</p>
         <p className="p2">¡En un CLICK!</p>
-        <img src="../../public/check.png" alt="check" className="checkImg"/>
+        <img src="images/check.png" alt="check" className="checkImg"/>
       </div>
     </div>
   );
